9361f263ae5705f8a58d993bcb2456c674fe01a4
Ian Macalinao <me@ian.pw>
2015-06-13 11:53:14 -0700

    setup sript

---
+++ android/setup/setup.sh
@@ -0,0 +1,17 @@
+@echo off
+
+echo Android AirInput -- Initializing...
+
+echo Waiting for device to be connected...
+adb wait-for-device
+
+echo - Installing native service...
+adb push airinput/dist/air-native "/data/local/tmp/air-native"
+adb shell chmod 755 "/data/local/tmp/air-native"
+
+echo Starting...
+adb shell kill -9 "/data/local/tmp/air-native"
+adb shell "/data/local/tmp/air-native -daemon"
+adb shell "/data/local/tmp/air-native"
+
+echo Service started successfully.
