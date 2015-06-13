3107e3b6562b4d0798e9a9cdd44d41f061272ae5
Ian Macalinao <me@ian.pw>
2015-06-13 10:28:26 -0700

    add touch command sender

--- android/app/src/main/java/pw/ian/hackwiththebest/TouchEmulator.java
+++ android/app/src/main/java/pw/ian/hackwiththebest/TouchEmulator.java
@@ -1,7 +1,25 @@
 package pw.ian.hackwiththebest;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 /**
  * Created by ian on 6/13/15.
  */
 public class TouchEmulator {
+    private void sendCommand(String cmd) {
+        try {
+            Process process = Runtime.getRuntime().exec("su");
+            DataOutputStream os = new DataOutputStream(process.getOutputStream());
+            os.writeBytes("/system/bin/input " + cmd);
+            os.writeBytes("exit\n");
+            os.flush();
+            os.close();
+            process.waitFor();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
