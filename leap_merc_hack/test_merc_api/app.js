ee1d672dca8691c4648802485ee2d5178170bbd2
Ian Macalinao <me@ian.pw>
2015-06-13 17:25:03 -0700

    add public dir

--- leap_merc_hack/test_merc_api/app.js
+++ leap_merc_hack/test_merc_api/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 let app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
