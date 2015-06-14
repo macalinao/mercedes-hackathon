4726ee82403aee80109ab6b1d4dd87cb4aad710f
Ian Macalinao <me@ian.pw>
2015-06-13 17:34:36 -0700

    request

--- leap_merc_hack/test_merc_api/app.js
+++ leap_merc_hack/test_merc_api/app.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import request from 'superagent-bluebird-promise';
 
 let app = express();
+
+// GhettoDB
+let data = [];
+data.pushSafe = function(e) {
+  this.push(e);
+  if (this.length > 1000) {
+    this.shift();
+  }
+};
 
 app.use(express.static(__dirname + '/public'));
 
